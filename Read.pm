73702048ba1e79ac85d230699485fb26ea25d06a
H.Merijn Brand <merijn@nb09.procura.nl>
2010-02-11 08:41:57 +0100

    Doc fix (RT#54507, Patrick Cronin)

--- Read.pm
+++ Read.pm
@@ -23,7 +23,7 @@ package Spreadsheet::Read;
 use strict;
 use warnings;
 
-our $VERSION = "0.38";
+our $VERSION = "0.39";
 sub  Version { $VERSION }
 
 use Carp;
@@ -904,8 +904,8 @@ If the constructor was called with C<attr> having a true value, effort
 is made to analyse and store field attributes like this:
 
     { label  => "Sheet 1",
-      maxrow => 2,
-      maxcol => 4,
+      maxrow => 5,
+      maxcol => 2,
       cell   => [ undef,
 	[ undef, 1 ],
 	[ undef, undef, undef, undef, undef, "Nugget" ],
