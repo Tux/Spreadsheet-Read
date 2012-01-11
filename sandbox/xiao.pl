0384b585061898025d5bf0daa2a78a9e31d29557
H.Merijn Brand <merijn@lx09.procura.nl>
2012-01-11 09:29:55 +0100

    xiao test case for encoding

---
+++ sandbox/xiao.pl
@@ -0,0 +1,15 @@
+#!/pro/bin/perl
+
+use 5.012;
+use warnings;
+
+use Data::Peek;
+use Spreadsheet::Read;
+use encoding "cp936", STDOUT => "cp936";
+
+my $ss = ReadData ("xiao.xls")->[2];
+
+DDumper { A3 => DDisplay ($ss->{A3}), 21 => DDisplay ($ss->{cell}[2][1]) };
+say $ss->{A3};
+say $ss->{cell}[2][1];
+
