1475badbe02b5226f6781f38be0ad8a50ab2578d
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2013-06-24 12:31:26 +0200

    ws spreadsheet format testing stuff

---
+++ sandbox/ws2csv.pl
@@ -0,0 +1,34 @@
+#!/pro/bin/perl
+
+use 5.16.2;
+use warnings;
+use autodie;
+
+use Text::CSV_XS;
+
+open my $fh, "<", $ARGV[0];
+
+my $header;
+read $fh, $header, 12;	# Really no idea
+
+my @data;
+
+# 0402 vvvv	- vvvv is length of field + control
+# rrrr cccc ???? llll
+my $buf;
+while (read $fh, $buf, 4) {
+
+    my ($h0, $fldlen) = unpack "vv", $buf;
+
+    $fldlen or next;
+
+    read $fh, $buf, $fldlen;
+
+    # x was always 0
+    my ($row, $col, $x, $val) = unpack "vv v v/a", $buf;
+
+    $data[$row][$col] = $val;
+    }
+
+my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1, eol => "\n" });
+$csv->print (*STDOUT, $_) for @data;
