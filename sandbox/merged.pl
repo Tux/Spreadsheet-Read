893eb79b573c5d88ab34c9ada7a4f18b25f4b1d1
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2015-05-12 11:27:54 +0200

    keep a test script

---
+++ sandbox/merged.pl
@@ -0,0 +1,21 @@
+#!/pro/bin/perl
+
+use 5.20.0;
+use warnings;
+use Spreadsheet::Read;
+
+my $ss = ReadData ("merged.xlsx", attr => 1, debug => 99)->[1];
+
+foreach my $row (1 .. $ss->{maxrow}) {
+    foreach my $col (1 .. $ss->{maxcol}) {
+	my $cell = cr2cell ($col, $row);
+	printf "%s %-6s %d  ", $cell, $ss->{$cell},
+	    $ss->{attr}[$col][$row]{merged};
+	}
+    print "\n";
+    }
+
+say for keys %$ss;
+foreach my $area (@{$ss->{merged}}) {
+    print cr2cell (@{$area}[0,1]), " -> ", cr2cell (@{$area}[2,3]), "\n";
+    }
