bcf679d0b17f5e1ffec32de830a0badcee9ff7eb
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2015-06-13 12:27:04 +0200

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -83,7 +83,7 @@ optional_features:
     recommends:
       Text::CSV:                           1.33
       Text::CSV_PP:                        1.33
-      Text::CSV_XS:                        1.16
+      Text::CSV_XS:                        1.18
   opt_excel:
     description:        Provides parsing of Microsoft Excel files
     requires:
