bb1797b650802145f25342700bf3a6928a3b659e
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2018-01-01 14:53:09 +0100

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -69,7 +69,7 @@ recommends:
   File::Temp:           0.2304
   Data::Peek:           0.47
 test_recommends:
-  Test::More:           1.302106
+  Test::More:           1.302120
 resources:
   license:              http://dev.perl.org/licenses/
   repository:           https://github.com/Tux/Spreadsheet-Read
